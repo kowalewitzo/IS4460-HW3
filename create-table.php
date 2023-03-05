9c4e65aa69cc414ffe2382b24895e24c51ee5061
kowalewitzo <125925029+kowalewitzo@users.noreply.github.com>
2023-03-05 21:55:49 +0100

    Add files via upload

---
+++ create-table.php
@@ -0,0 +1,15 @@
+<?php
+create table user (
+   id (int(11), auto-increment)
+   username varchar(128),
+   forename varchar(128)
+   surname varchar(128)
+   password varchar(255)
+);
+
+?>
+
+
+
+
+
---
+++ user-details.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once  'login.php';
+
+$conn = new mysqli($hn, $un, $pw, $db);
+if($conn->connect_error) die($conn->connect_error);
+
+$query = "SELECT * FROM user";
+
+$result = $conn->query($query); 
+if(!$result) die($conn->error);
+
+$rows = $result->num_rows;
+
+for($j=0; $j<$rows; $j++)
+{
+	//$result->data_seek($j); 
+	$row = $result->fetch_array(MYSQLI_ASSOC); 
+
+echo <<<_END
+	<pre>
+	<a>Username: $row[username]
+	Forename: $row[forename]
+	Surname: $row[surname]
+	Password: $row[password]
+	Id: $row[id]</a>
+	</pre>
+	
+	<form action='deleteRecord.php' method='post'>
+		<input type='hidden' name='delete' value='yes'>
+		<input type='hidden' name='id' value='$row[id]'>
+		<input type='submit' value='DELETE RECORD'>	
+	</form>
+	
+_END;
+
+}
+
+$conn->close();
+
+
+
+?>
