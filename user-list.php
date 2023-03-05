b557a71098194a8dd678ed8ee56dda1cb6cd0489
kowalewitzo <125925029+kowalewitzo@users.noreply.github.com>
2023-03-05 23:46:31 +0100

    Add files via upload

---
+++ user-list.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+ 		<title>Users</title>
+
+	</head>
+	<body>
+		<H1>Users</H1>
+
+		<form action='http://localhost:8888/HW3/add-user.php'>
+		<button class='button'>Add User</button>
+		</form>
+
+		<b> User 1: </b>
+		</p><p><a href='http://localhost:8888/HW3/user-details.php'>kowalewitzo</a><br><br>
+		<b> User 2: </b></p><p><a href='http://localhost:8888/HW3/user-details.php'>lampkinz</a><br><br>
+		<b> User 3: </b></p><p><a href='http://localhost:8888/HW3/user-details.php'>younga</a><br><br>
+		<b> User 4: </b></p><p><a href='http://localhost:8888/HW3/user-details.php'>starkj</a><br><br>
+		<b> User 5: </b></p><p><a href='http://localhost:8888/HW3/user-details.php'>ryndersg</a><br><br>
+		<b> User 6: </b></p><p><a href='http://localhost:8888/HW3/user-details.php'>coffmanl</a><br><br>
+		<b> User 7: </b></p><p><a href='http://localhost:8888/HW3/user-details.php'>hoffmanr</a><br><br>
+		<b> User 8: </b></p><p><a href='http://localhost:8888/HW3/user-details.php'>kelleyt</a><br><br>
+		<b> User 9: </b></p><p><a href='http://localhost:8888/HW3/user-details.php'>feolom</a><br><br>
+		<b> User 10: </b></p><p><a href='http://localhost:8888/HW3/user-details.php'>saylorb</a>
+		
+		<form action='http://localhost:8888/HW3/login-form.php'>
+		<button class='button'>Return to Login</button>
+		</form>
+	</body>
+</html>
